e2a93e6cb2a6839042cae8eec77fedc0f77ad319
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    addison: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- device.mk
+++ device.mk
@@ -36,8 +36,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aov_ec_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aov_ec_mixer_paths.xml \
