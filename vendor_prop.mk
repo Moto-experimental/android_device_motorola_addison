4421479a4b48dd10fd19068fe1fa940bb197827c
Jorim Jaggi <jjaggi@google.com>
2021-09-09 23:06:30 +0400

    addison: Disable sdm rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.
    
    Test: Open app, no jank
    Bug: 73640835
    Change-Id: I6ea9baa28acc222afcf365f3bf107149bc8974d9
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -89,6 +89,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.disable_skip_validate=1 \
+    vendor.display.disable_rotator_downscale=1 \
     vendor.video.disable.ubwc=1 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
