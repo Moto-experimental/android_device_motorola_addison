07c97ab5b7f16acb8654caf4baba8ca43cf165c5
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-18 17:44:47 +0800

    addison: Ship cgroups.json and task_profiles.json
    Google's prebuilt cgroups.json and task_profiles.json for products
    launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    These devices were launched with M (API level 23), so schedtune
    and task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I65fe622a804a720a97b43d8092cc8a37703f4b64

--- device.mk
+++ device.mk
@@ -307,6 +307,10 @@ PRODUCT_COPY_FILES += \
 # Properties
 include device/motorola/addison/vendor_prop.mk
 
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
