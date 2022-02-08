eb814ccd53e7cdda6978ac3c343e29d988a6a705
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    addison: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device.mk
+++ device.mk
@@ -30,7 +30,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@2.0-impl \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaacwrapper \
