7bc15883279dde4afe4e57505d130a691b1fc9da
SirRGB <sirrgb@proton.me>
2026-05-26 17:57:24 +0200

    addison: use kernel defconfig fragments
    Change-Id: I39152c81f985880a3198b479b5e2ccc7a332def6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,9 +106,9 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := addison_defconfig
+TARGET_KERNEL_CONFIG += vendor/moto/addison.config
+TARGET_KERNEL_RECOVERY_CONFIG += vendor/moto/addison.config
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
-TARGET_KERNEL_RECOVERY_CONFIG := recovery_addison_defconfig
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
